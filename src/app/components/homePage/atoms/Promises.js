45a2f9820ddec65ce10b72a796c4b5f3c3d8c209
Johan Nordland <johan_myhre@outlook.com>
2025-08-10 18:08:23 +0200

    Improve responsive scaling for Promises and MainBanner text

--- src/app/components/homePage/atoms/Promises.js
+++ src/app/components/homePage/atoms/Promises.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Promises = () => {
   return (
-    <div className="mt-0 pt-0 mx-auto flex flex-wrap justify-center gap-4 text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl [@media(min-width:2000px)]:text-3xl text-white text-yellow-300 min-w-96 [@media(min-width:2000px)]:min-w-[50vw]">
+    <div className="mt-0 pt-0 mx-auto flex flex-wrap justify-center gap-2 sm:gap-3 md:gap-4 text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl [@media(min-width:2000px)]:text-3xl text-white text-yellow-300 min-w-96 [@media(min-width:2000px)]:min-w-[50vw]">
       <div className="px-2">[✔] Gratis frakt over 2000 kr</div>
       <div className="px-2">[✔] 50 dagers åpent kjøp og fri retur</div>
       <div className="px-2">[✔] Norsk håndverk</div>
